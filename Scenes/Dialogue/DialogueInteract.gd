a70d3fc672438e23ca1c9c37728f7b4727190a11
Sam Hansen <samowenhansen@gmail.com>
2021-01-30 14:24:01 -0800

    dialogue system wip

---
+++ Scenes/Dialogue/DialogueInteract.gd
@@ -0,0 +1,15 @@
+extends Node
+
+export (String, FILE, "*.json") var dialogue_path
+
+func onInteract():
+	var dialogue = load_from_json(dialogue_path)
+
+func load_from_json(dialogue_path):
+	var file = File.new()
+	assert(file.file_exists(dialogue_path))
+	file.open(dialogue_path, file.READ)
+	var dialogue = parse_json(file.get_as_text())
+	assert(dialogue.size() > 0)
+	file.close()
+	return dialogue
