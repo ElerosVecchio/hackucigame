3e55325413b57ca08b53c8e9bd2b1468f08fbb97
Sam Hansen <samowenhansen@gmail.com>
2021-01-31 03:37:24 -0800

    global singleton and cursor shit

---
+++ Globabl.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var hasCar = false
+var hasChimney = false
+var hasComputer = false
+var hasConveyor = false
+var hasStump = false
+var hasHabitat = false
+var hasSoil = false
+var hasAnimal = false
+var hasOcean = false
+var hasSewage = false
+var hasGarbage = false
+var hasTractor = false
+var hasCRT = false
